f3330272609cdec7f44ebbbd97fd846931ccb139
gruppiv <matheusgruppi@gmail.com>
2020-08-08 13:33:18 -0300

    Lack of courses made

---
+++ swirl_7_8_14.R
@@ -0,0 +1,49 @@
+library("swirl")
+swirl()
+gruppiv
+3
+1
+1
+8
+TRUE == TRUE
+(FALSE == TRUE) == FALSE
+6 == 7
+6 < 7
+10 <= 10
+4
+1
+5 != 7
+!(5 == 7)
+3
+4
+FALSE & FALSE
+TRUE & c(TRUE, FALSE, FALSE)
+TRUE && c(TRUE, FALSE, FALSE)
+TRUE | c(TRUE, FALSE, FALSE)
+TRUE || c(TRUE, FALSE, FALSE)
+5 > 8 || 6 != 8 && 4 > 3.9
+4
+4
+isTRUE(6 > 4)
+identical('twins', "twins")
+3
+xor(5 == 6, !FALSE)
+3
+3
+4
+ints <- sample(10)
+ints
+ints > 5
+which(ints > 7)
+2
+any(ints < 0)
+all(ints > 0)
+3
+2
+matheusgruppi@gmail.com
+LN1miWlFQ5YVT3IY
+1
+9
+Sys.Date()
+mean(c(2, 4, 5))
+
